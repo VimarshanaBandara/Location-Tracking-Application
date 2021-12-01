7f44a5f70129acb0656efa74c731a2e825dcf772
VimarshanaBandara <vimarshanabandara76@gmail.com>
2021-12-01 13:00:31 +0530

    Map API

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,23 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:location_tracking_app/blocs/application_blocs.dart';
 import 'package:location_tracking_app/screens/homeScreen.dart';
 import 'package:provider/provider.dart';
 
-void main(){
-  runApp(MyApp());
+void main() {
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
-      create: (context)=> ApplicationBloc(),
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
+      create: (context) => ApplicationBloc(),
+      child: const MaterialApp(
+        title: 'Flutter Demo',
         home: HomeScreen(),
       ),
     );
   }
 }
-
-
-
-
---
+++ lib/models/geometry.dart
@@ -0,0 +1,11 @@
+import 'package:location_tracking_app/models/location.dart';
+
+
+class Geometry {
+  final Location location;
+
+  Geometry({required this.location});
+
+  Geometry.fromJson(Map<dynamic,dynamic> parsedJson)
+      :location = Location.fromJson(parsedJson['location']);
+}
